a2458c6a4de303a92bd0b5637266290cab346a4f
Emily Stamey <elstamey@ncsu.edu>
2016-04-26 19:21:21 -0400

    Completed Introduction Part One exercises

---
+++ HelloWorld.py
@@ -0,0 +1,14 @@
+__author__ = 'dev'
+print('Hello, World!')
+print(1+2)
+print(7*6)
+print()
+print('The End')
+print("Python's strings are easy to use")
+print('We can even use "quotes" in strings')
+
+greeting = "Hello"
+name = "Bruce"
+print(greeting + name)
+# if we want a space, we can add that too
+print(greeting + ', ' + name)
