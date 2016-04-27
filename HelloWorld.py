8f887059c62e02aac103386cd2a15b5d6125e043
Emily Stamey <elstamey@ncsu.edu>
2016-04-26 20:21:37 -0400

    Completed Introduction Part Two exercises

--- HelloWorld.py
+++ HelloWorld.py
@@ -1,14 +1,22 @@
 __author__ = 'dev'
-print('Hello, World!')
-print(1+2)
-print(7*6)
-print()
-print('The End')
-print("Python's strings are easy to use")
-print('We can even use "quotes" in strings')
-
 greeting = "Hello"
-name = "Bruce"
-print(greeting + name)
-# if we want a space, we can add that too
+name = input("Please enter your name ")
+
 print(greeting + ', ' + name)
+
+splitString = "This string has been\nsplit over\nseveral\nlines"
+print(splitString)
+
+tabbedString = "1\t2\t3\t4\t5\t"
+print(tabbedString)
+
+print('The pet shop owner said "No, No, \'e\'s uh,...he\'s resting"')
+print("The pet shop owner said \"No, No, 'e's uh,...he's resting\"")
+
+anotherSplitString = """This string has been
+split over
+several lines"""
+print(anotherSplitString)
+
+print('''The pet shop owner said "No, No, 'e's uh,...he's resting"''')
+print("""The pet shop owner said "No, No, 'e's uh,...he's resting" """)
