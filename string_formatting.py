b8db05c1968a620a44e642139bf4a4a636d869c1
Emily Stamey <elstamey@ncsu.edu>
2016-04-30 18:57:54 -0400

    Completed String Formatting exercises

---
+++ string_formatting.py
@@ -0,0 +1,20 @@
+__author__ = 'dev'
+
+age = 40
+print("my age is " + str(age) + " years")
+
+print("My age is {0} years".format(age))
+
+for i in range(1, 12):
+    print("No. %2d squared is %4d and cubed is %4d" %(i, i ** 2, i ** 3))
+
+print("Pi is approximately %12.50f" %(22/7))
+
+# {x:y} format with the number being x digits and leaving y spaces
+for i in range(1, 12):
+    print("No. {0:2} squared is {1:4} and cubed is {2:4}".format(i, i ** 2, i ** 3))
+
+print("Pi is approximately {0:12.50}".format(22/7))
+
+for i in range(1, 12):
+    print("No. {} squared is {} and cubed is {:4}".format(i, i ** 2, i ** 3))
