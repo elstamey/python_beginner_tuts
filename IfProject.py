4911c005ccfb8c05729e3e74330507d9f5af14c9
Emily Stamey <elstamey@ncsu.edu>
2016-04-30 20:04:48 -0400

    Streamlined some of that ugly If

--- IfProject.py
+++ IfProject.py
@@ -13,15 +13,12 @@ __author__ = 'dev'
 print("Please guess a number between 1 and 10: ")
 guess = int(input())
 
-if guess < 5:
-    print("Please guess higher")
-    guess = int(input())
-    if guess == 5:
-        print("Well done, you guessed it")
+if guess != 5:
+    if guess < 5:
+        print("Please guess higher")
     else:
-        print("Sorry, you have not guessed correctly")
-elif guess > 5:
-    print("Please guess lower")
+        print("Please guess lower")
+        
     guess = int(input())
     if guess == 5:
         print("Well done, you guessed it")
